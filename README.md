e5086c470ab6517b810c3bc4713a677b802354c5
kayibea <kayibea@proton.me>
2026-01-08 06:00:44 +0100

    chore: first commit

---
+++ README.md
@@ -0,0 +1,33 @@
+# Gopher TOTP
+
+A minimal CLI to generate TOTP codes from a Base32 secret.
+
+* Reads the TOTP secret from stdin.
+* Prints the 6-digit code, formatted `XXX-XXX`.
+* Automatically updates every 30s.
+* Copies the current code to the clipboard (if available).
+
+## Usage
+
+```bash
+echo 'JBSWY3DPEHPK3PXP' | gopher-totp
+```
+```
+Ctrl+C to exit.
+Expires in: 13s | Code: 944-391
+```
+
+## GNU Pass Integration
+
+You can pipe a TOTP secret stored in [pass](www.passwordstore.org) directly into `gopher-totp`:
+
+```bash
+pass show totp/example.com | sed -n '1p' | gopher-totp
+```
+
+Or create a convenient alias:
+
+```bash
+alias pass-totp='sed -n '1p' | gopher-totp'
+pass show totp/example.com | pass-totp
+```
---
+++ main.go
@@ -0,0 +1,116 @@
+package main
+
+import (
+	"bufio"
+	"crypto/hmac"
+	"crypto/sha1"
+	"encoding/base32"
+	"encoding/binary"
+	"fmt"
+	"golang.design/x/clipboard"
+	"os"
+	"strconv"
+	"strings"
+	"time"
+)
+
+const (
+	step   = 30
+	digits = 6
+)
+
+var totpMod uint32 = 1
+
+func init() {
+	for range digits {
+		totpMod *= 10
+	}
+}
+
+func totp(secret []byte, t time.Time) uint32 {
+	counter := uint64(t.Unix() / step)
+
+	var buf [8]byte
+	binary.BigEndian.PutUint64(buf[:], counter)
+
+	mac := hmac.New(sha1.New, secret)
+	mac.Write(buf[:])
+	sum := mac.Sum(nil)
+
+	offset := sum[len(sum)-1] & 0x0f
+	bin := (uint32(sum[offset])&0x7f)<<24 |
+		(uint32(sum[offset+1])&0xff)<<16 |
+		(uint32(sum[offset+2])&0xff)<<8 |
+		(uint32(sum[offset+3]) & 0xff)
+
+	return bin % totpMod
+}
+
+func printUsage() {
+	fmt.Printf("Usage: %s\n", os.Args[0])
+	fmt.Println("Reads the TOTP secret from stdin and prints the code every 30s.")
+	fmt.Println("The current code is automatically copied to the clipboard while the program is running.")
+	fmt.Println("Example:")
+	fmt.Printf("  echo 'JBSWY3DPEHPK3PXP' | %s\n", os.Args[0])
+}
+
+func main() {
+	if len(os.Args) > 1 {
+		printUsage()
+		os.Exit(1)
+	}
+
+	in := bufio.NewReader(os.Stdin)
+	secretStr, err := in.ReadString('\n')
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "failed to read secret from stdin: %v\n", err)
+		os.Exit(1)
+	}
+
+	secretStr = strings.ReplaceAll(secretStr, " ", "")
+	secretStr = strings.ToUpper(secretStr)
+	secret, err := base32.StdEncoding.WithPadding(base32.NoPadding).DecodeString(secretStr)
+	if err != nil {
+		fmt.Fprintln(os.Stderr, "invalid base32 secret")
+		os.Exit(1)
+	}
+
+	var clipboardOk bool = true
+	var lastCounter uint64 = ^uint64(0)
+	var currentCode uint32
+
+	err = clipboard.Init()
+	if err != nil {
+		clipboardOk = false
+		fmt.Printf("Warning: clipboard unavailable (%v)\n", err)
+	}
+
+	fmt.Println("Ctrl+C to exit.")
+	for {
+		now := time.Now()
+		counter := uint64(now.Unix() / step)
+
+		if counter != lastCounter {
+			lastCounter = counter
+			currentCode = totp(secret, now)
+
+			if clipboardOk {
+				clipboard.Write(
+					clipboard.FmtText,
+					[]byte(strconv.FormatUint(uint64(currentCode), 10)),
+				)
+			}
+
+		}
+
+		remaining := step - int(now.Unix()%step)
+
+		codeStr := fmt.Sprintf("%0*d", digits, currentCode)
+		readable := fmt.Sprintf("%s-%s", codeStr[:3], codeStr[3:])
+		fmt.Printf("\rExpires in: %2ds | Code: %s", remaining, readable)
+
+		os.Stdout.Sync()
+
+		time.Sleep(1 * time.Second)
+	}
+}
